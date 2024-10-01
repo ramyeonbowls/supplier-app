e18f4f4f9269537de3b2a5438ed20554009b0f7f
Julian <julian@eskalink.com>
2024-10-01 18:41:20 +0700

    update

--- resources/js/login/App.vue
+++ resources/js/login/App.vue
@@ -10,7 +10,7 @@
                     <p class="text-center text-lg font-medium">Sign in to your account</p>
 
                     <div>
-                        <label for="Username" class="relative block rounded-md border border-gray-200 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600">
+                        <label for="email" class="relative block rounded-md border border-gray-200 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600">
                             <input type="email" id="email" class="peer border-none bg-transparent placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0" placeholder="Enter email" name="email" v-model="form.field.email" required autocomplete="email" autofocus />
 
                             <span class="pointer-events-none absolute start-2.5 top-0 -translate-y-1/2 bg-white p-0.5 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs">Enter email</span>
