e2a041744946998d5e9241e09472a76fe164640e
Julian Putra <julian.putr99@gmail.com>
2024-11-15 00:48:24 +0700

    update

--- resources/js/home/components/EncryptBooks.vue
+++ resources/js/home/components/EncryptBooks.vue
@@ -185,41 +185,23 @@
                                                             <a href="javascript:void(0)" class="whitespace-nowrap" @click="showSinopsis(row[header.key])">{{ row[header.key].substring(0, 10) + '...' }}</a>
                                                         </template>
                                                         <template v-else-if="header.key == 'status'">
-                                                            <span v-if="row[header.key] == '1'" class="inline-flex items-center justify-center rounded-full bg-slate-100 px-2.5 py-0.5 text-slate-700">
-                                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="-ms-1 me-1.5 size-4">
-                                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
-                                                                </svg>
-                                                                <p class="whitespace-nowrap text-sm">Draft</p>
+                                                            <span v-if="row[header.key] == '1'">
+                                                                <p class="whitespace-nowrap text-sm text-slate-700">Draft</p>
                                                             </span>
-                                                            <span v-else-if="row[header.key] == '2'" class="inline-flex items-center justify-center rounded-full bg-amber-100 px-2.5 py-0.5 text-amber-700">
-                                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="-ms-1 me-1.5 size-4">
-                                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
-                                                                </svg>
-                                                                <p class="whitespace-nowrap text-sm">Review</p>
+                                                            <span v-else-if="row[header.key] == '2'">
+                                                                <p class="whitespace-nowrap text-sm text-amber-700">Review</p>
                                                             </span>
-                                                            <span v-else-if="row[header.key] == '3'" class="inline-flex items-center justify-center rounded-full bg-emerald-100 px-2.5 py-0.5 text-emerald-700">
-                                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="-ms-1 me-1.5 size-4">
-                                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
-                                                                </svg>
-                                                                <p class="whitespace-nowrap text-sm">Publish</p>
+                                                            <span v-else-if="row[header.key] == '3'">
+                                                                <p class="whitespace-nowrap text-sm text-emerald-700">Publish</p>
                                                             </span>
-                                                            <span v-else-if="row[header.key] == '4'" class="inline-flex items-center justify-center rounded-full bg-lime-100 px-2.5 py-0.5 text-lime-700">
-                                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="-ms-1 me-1.5 size-4">
-                                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
-                                                                </svg>
-                                                                <p class="whitespace-nowrap text-sm">Publish pending</p>
+                                                            <span v-else-if="row[header.key] == '4'">
+                                                                <p class="whitespace-nowrap text-sm text-lime-700">Publish pending</p>
                                                             </span>
-                                                            <span v-else-if="row[header.key] == '5'" class="inline-flex items-center justify-center rounded-full bg-rose-100 px-2.5 py-0.5 text-rose-700">
-                                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="-ms-1 me-1.5 size-4">
-                                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
-                                                                </svg>
-                                                                <p class="whitespace-nowrap text-sm">Reject</p>
+                                                            <span v-else-if="row[header.key] == '5'">
+                                                                <p class="whitespace-nowrap text-sm text-rose-700">Reject</p>
                                                             </span>
-                                                            <span v-else-if="row[header.key] == '5'" class="inline-flex items-center justify-center rounded-full bg-stone-100 px-2.5 py-0.5 text-stone-700">
-                                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="-ms-1 me-1.5 size-4">
-                                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
-                                                                </svg>
-                                                                <p class="whitespace-nowrap text-sm">Ditarik</p>
+                                                            <span v-else-if="row[header.key] == '5'">
+                                                                <p class="whitespace-nowrap text-sm text-stone-700">Ditarik</p>
                                                             </span>
                                                         </template>
                                                         <template v-else>
