a6cb25ece10cc6bd78648d5cddf48c595c9c442d
Julian Putra <julian.putr99@gmail.com>
2024-11-16 18:28:50 +0700

    update

--- resources/js/home/components/MenuSupplier.vue
+++ resources/js/home/components/MenuSupplier.vue
@@ -37,7 +37,7 @@
                 <p class="text-sm font-semibold text-slate-500 group-hover:text-slate-50">Kategori Buku</p>
             </li>
         </router-link>
-        <router-link :to="{ name: 'encrypt_file' }">
+        <!-- <router-link :to="{ name: 'encrypt_file' }">
             <li class="group flex items-center gap-3 rounded-md p-2 hover:cursor-pointer hover:bg-indigo-500 focus:bg-indigo-600 active:bg-indigo-800">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="" class="size-5 stroke-svg-icon-color group-hover:stroke-white">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.75V12A2.25 2.25 0 0 1 4.5 9.75h15A2.25 2.25 0 0 1 21.75 12v.75m-8.69-6.44-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
@@ -45,7 +45,7 @@
 
                 <p class="text-sm font-semibold text-slate-500 group-hover:text-slate-50">Enkripsi File</p>
             </li>
-        </router-link>
+        </router-link> -->
     </ul>
     <slot></slot>
 </template>
