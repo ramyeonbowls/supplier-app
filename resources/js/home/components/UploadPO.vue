a51661c1f82bdc6afbdc518eee36f8974f8f536e
Julian Putra <julian.putr99@gmail.com>
2024-12-24 22:53:39 +0700

    Update UploadPO component to change status label from 'Belum Lunas' to 'Dikirim ke Supplier' and modify background color for status 3

--- resources/js/home/components/UploadPO.vue
+++ resources/js/home/components/UploadPO.vue
@@ -139,11 +139,11 @@
                                                             </svg>
                                                             <p class="whitespace-nowrap text-sm">Approved</p>
                                                         </span>
-                                                        <span v-if="row['status'] == '3'" class="inline-flex items-center justify-center rounded-full bg-slate-100 px-2.5 py-0.5 text-slate-700">
+                                                        <span v-if="row['status'] == '3'" class="inline-flex items-center justify-center rounded-full bg-amber-100 px-2.5 py-0.5 text-amber-700">
                                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="-ms-1 me-1.5 size-4">
                                                                 <path stroke-linecap="round" stroke-linejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
                                                             </svg>
-                                                            <p class="whitespace-nowrap text-sm">Belum Lunas</p>
+                                                            <p class="whitespace-nowrap text-sm">Dikirim ke Supplier</p>
                                                         </span>
                                                         <span v-if="row['status'] == '4'" class="inline-flex items-center justify-center rounded-full bg-emerald-100 px-2.5 py-0.5 text-emerald-700">
                                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="-ms-1 me-1.5 size-4">
