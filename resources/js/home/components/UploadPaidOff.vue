e4062bbd81f7b4bd18f5dabfa1d62360a5c86d59
Julian Putra <julian.putr99@gmail.com>
2024-12-25 01:03:56 +0700

    Refactor dialog panel styles in ReportPO and UploadPaidOff components for improved responsiveness

--- resources/js/home/components/UploadPaidOff.vue
+++ resources/js/home/components/UploadPaidOff.vue
@@ -290,13 +290,13 @@
             <div class="fixed inset-0 z-50 w-screen overflow-y-auto">
                 <div class="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
                     <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
-                        <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all" :style="{ width: 'auto', maxWidth: '100%' }">
+                        <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-1">
                             <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                                 <div class="sm:flex sm:items-center">
                                     <div class="text-center sm:text-left">
                                         <DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900">Bukti Pembayaran</DialogTitle>
                                         <div class="mt-2 flex justify-center p-1">
-                                            <img :src="form.image_url" class="rounded-lg shadow-md" :style="{ height: '28.125rem', width: '18.75rem' }" alt="Preview" />
+                                            <img :src="form.image_url" class="rounded-lg shadow-md sm:max-h-[28rem]" alt="Preview" />
                                         </div>
                                     </div>
                                 </div>
